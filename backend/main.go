c9dd1751307d7c7de72842063e61b7bc859c39cf
Jonathan <lawjonathan17@gmail.com>
2023-04-14 15:07:34 -0700

    Setup

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+// Retrieve Asanas (poses) from data store
+// func getAsanas() {
+
+// }
+
+func Router() http.Handler {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple server")
+	})
+
+	router.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Fprintf(w, `{"some": "test"}`)
+	})
+	return router
+}
 
 func main() {
+	c := cors.AllowAll()
+
 	fmt.Println("Namaste Healthy")
+	// router()
+	// http.ListenAndServe(":8080", nil)
+
+	handler := c.Handler(Router())
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+	srv.ListenAndServe()
 }
