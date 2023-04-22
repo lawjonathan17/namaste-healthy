b98d6587017aba3225906bc951604c27dfb899cc
Jonathan <lawjonathan17@gmail.com>
2023-04-22 14:23:01 -0700

    Update box breathing text

--- namaste-fe/src/pages/meditation.tsx
+++ namaste-fe/src/pages/meditation.tsx
@@ -8,7 +8,7 @@ import { $Body, $Description, $Header, $Text, $Topic } from "./common"
 // submod
 export function Meditation() {
 
-    const renderBoxBreathing = () => {
+    const renderPranayama = () => {
         return (
             <>
                 <$Topic>
@@ -27,31 +27,61 @@ export function Meditation() {
                 <br/>
                 <br/>
 
+                <$Topic>
+                    Benefits
+                </$Topic>
+
+                {/* <div> */}
+                    <ul>
+                        <li>Lowers stress</li>
+                    </ul>
+                {/* </div> */}
+            </>
+        )
+    }
+
+
+    const renderBoxBreathing = () => {
+        return (
+            <>
                 <$Description>
                     Box Breathing <i>(sama vritti pranayama)</i>
                 </$Description>
 
+                <$Text>
+                    Technique used to promote relaxation and reduce stress and anxiety. Take slow, deep breaths while visualizing the breath moving in a box pattern.
+                </$Text>
+                <br/>
+                <$Text>
+                    Find a comfortable seated position and close your eyes.
+                    <br/>
+                    Inhale deeply through your nose for a count of four.
+                    <br/>
+                    Hold your breath for a count of four.
+                    <br/>
+                    Exhale slowly through your mouth for a count of four.
+                    <br/>
+                    Hold your breath again for a count of four.
+                    <br/>
+                    Repeat steps 2-5 for several minutes, focusing on the square pattern of your breath.
+                </$Text>
 
-                <div>
-                    <ul>
-                        <li>Lowers stress</li>
-                    </ul>
-                </div>
-
-                <div className="box-wrapper">
-                    <div className="box-inhale box-text">Inhale</div>
-                    <div className="inhale-hold box-text">Hold</div>
-                    <div className="box-exhale box-text">Exhale</div>
-                    <div className="exhale-hold box-text">Hold</div>
-                    <div className="box">
-                        <span className="top"></span>
-                        <span className="right"></span>
-                        <span className="bottom"></span>
-                        <span className="left"></span>
+                <div className="box-container">
+                    <div className="box-wrapper">
+                        <div className="box-inhale box-text">Inhale</div>
+                        <div className="inhale-hold box-text">Hold</div>
+                        <div className="box-exhale box-text">Exhale</div>
+                        <div className="exhale-hold box-text">Hold</div>
+                        <div className="box">
+                            <span className="top"></span>
+                            <span className="right"></span>
+                            <span className="bottom"></span>
+                            <span className="left"></span>
+                        </div>
                     </div>
                 </div>
-            </>
 
+            </>
         )
     }
 
@@ -61,6 +91,7 @@ export function Meditation() {
             <$Body>
                 <$Header>Meditation</$Header>
 
+                {renderPranayama()}
                 {renderBoxBreathing()}
             </$Body>
 
